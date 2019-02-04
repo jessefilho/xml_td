9515e624287bb987a423668e9250b785f8df70b0
Jesse Ferreira Filho <jessefilhojfnf@gmail.com>
2019-02-04 09:41:15 +0100

    done, just needs some vqlidqtions

---
+++ src/main/java/TD1/XSLT_DOM_Q6.java
@@ -0,0 +1,54 @@
+package TD1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+
+
+public class XSLT_DOM_Q6 {
+	
+	public static void main(String[] args) {
+		String xmlInputFile = "bd.xml";
+		String xsltInputFile = "bd_jfnf.xsl";
+		String xmlOutoutFile = "jfnf.html";
+		
+		try {
+			//XML Source
+			Source xmlSource = new StreamSource(new FileInputStream(xmlInputFile));
+			//XSL Source
+			Source xsltSource = new StreamSource(new FileInputStream(xsltInputFile));
+			Result transResult = new StreamResult(new FileOutputStream(xmlOutoutFile));
+			
+			
+			TransformerFactory tf = TransformerFactory.newInstance();
+			try {
+				Transformer t = tf.newTransformer(xsltSource);
+				try {
+					t.transform(xmlSource, transResult);
+				} catch (TransformerException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} catch (TransformerConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
